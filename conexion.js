871faae41edf6d4289ead68cc2ac9e76ffd8bfea
paatshala1 <hugo.rodriguezz@icloud.com>
2021-11-21 20:49:49 -0600

    mongoCompass

--- conexion.js
+++ conexion.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 // let userArgs = require('./populatedb');
 // let uri = 'mongodb://127.0.0.1:27017/'.concat(userArgs);
-const uri = 'mongodb://127.0.0.1:27017/library';
+// mongodb+srv://Sofi:<Hibou>@cluster0.hdhaw.mongodb.net/library?retryWrites=true&w=majority
+// mongodb://127.0.0.1:27017/library
+
+const uri = 'mongodb+srv://Sofi:Hibou@cluster0.hdhaw.mongodb.net/library?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
